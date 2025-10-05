75bb8264abd325812963df574101894d909be5fb
Henry Moore <henrygerardmoore@gmail.com>
2025-10-04 22:26:02 -0600

    fill out MPC skeleton and put desired types

--- components/tasks/cu_mpc/src/lib.rs
+++ components/tasks/cu_mpc/src/lib.rs
@@ -3,119 +3,110 @@ use bincode::enc::Encoder;
 use bincode::error::{DecodeError, EncodeError};
 use bincode::{Decode, Encode};
 use cu29::prelude::*;
+use optimization_engine::constraints::Constraint;
+use optimization_engine::panoc::PANOCCache;
 use serde::Serialize;
 use std::marker::PhantomData;
 
 /// Output of the MPC controller.
 #[derive(Debug, Default, Clone, Encode, Decode, Serialize)]
 pub struct MPCControlOutputPayload {
-    /// Proportional term
-    pub p: f32,
-    /// Integral term
-    pub i: f32,
-    /// Derivative term
-    pub d: f32,
     /// Final output
-    pub output: f32,
+    pub output: Vec<f64>,
 }
 
 /// This is the underlying standard MPC controller.
-pub struct MPCController {
-    // Configuration
-    kp: f32,
-    ki: f32,
-    kd: f32,
-    setpoint: f32,
-    p_limit: f32,
-    i_limit: f32,
-    d_limit: f32,
-    output_limit: f32,
-    sampling: CuDuration,
+pub struct MPCController<const N: usize> {
+    setpoint: [f64; N],
+    output_limits: [(f64, f64); N],
+    sample_period: CuDuration,
+    // f(x, u) -> xdot
+    dynamics_function: Box<dyn Fn(&[f64], &[f64]) -> [f64] + Send>,
+
+    // MPC controller must have at least one of the below cost functions
+    // optional state cost function, J(x, u) -> f64
+    state_cost: Option<Box<dyn Fn(&[f64], &[f64]) -> f64 + Send>>,
+
+    // optional terminal cost function, J(x) -> f64
+    terminal_cost: Option<Box<dyn Fn(&[f64]) -> f64 + Send>>,
+
+    constraint: Box<dyn Constraint>,
+
     // Internal state
-    integral: f32,
-    last_error: f32,
+    tolerance: f64,
+    cache: PANOCCache,
+    last_error: [f64; N],
     elapsed: CuDuration,
     last_output: MPCControlOutputPayload,
 }
 
-impl MPCController {
+impl<const N: usize> MPCController<N> {
     #[allow(clippy::too_many_arguments)]
     pub fn new(
-        kp: f32,
-        ki: f32,
-        kd: f32,
-        setpoint: f32,
-        p_limit: f32,
-        i_limit: f32,
-        d_limit: f32,
-        output_limit: f32,
-        sampling: CuDuration, // to avoid oversampling and get a bunch of zeros.
+        setpoint: [f64; N],
+        output_limits: [(f64, f64); N],
+        sample_period: CuDuration,
+        dynamics_function: impl Fn(&[f64], &[f64]) -> [f64] + Send + 'static,
+        state_cost: Option<impl Fn(&[f64], &[f64]) -> f64 + Send + 'static>,
+        terminal_cost: Option<impl Fn(&[f64]) -> f64 + Send + 'static>,
+        constraint: impl Constraint + Send + 'static,
+        tolerance: f64,
     ) -> Self {
         MPCController {
-            kp,
-            ki,
-            kd,
             setpoint,
-            integral: 0.0,
-            last_error: 0.0,
-            p_limit,
-            i_limit,
-            d_limit,
-            output_limit,
+            output_limits,
+            sample_period,
+            dynamics_function: Box::new(dynamics_function),
+            state_cost: state_cost.map(|state_cost_function| {
+                Box::new(state_cost_function) as Box<dyn Fn(&[f64], &[f64]) -> f64 + Send>
+            }),
+            terminal_cost: terminal_cost.map(|terminal_cost_function| {
+                Box::new(terminal_cost_function) as Box<dyn Fn(&[f64]) -> f64 + Send>
+            }),
+            constraint: Box::new(constraint),
+            cache: PANOCCache::new(N, tolerance, 20),
             elapsed: CuDuration::default(),
-            sampling,
             last_output: MPCControlOutputPayload::default(),
+            last_error: [0.0; N],
+            tolerance: tolerance,
         }
     }
 
     pub fn reset(&mut self) {
-        self.integral = 0.0f32;
-        self.last_error = 0.0f32;
+        self.cache = PANOCCache::new(N, self.tolerance, 20);
+        self.last_error = [0.0; N];
     }
 
-    pub fn init_measurement(&mut self, measurement: f32) {
-        self.last_error = self.setpoint - measurement;
-        self.elapsed = self.sampling; // force the computation on the first next_control_output
+    pub fn init_measurement(&mut self, measurement: &[f64; N]) {
+        self.update_error(measurement);
+        self.elapsed = self.sample_period; // force the computation on the first next_control_output
+    }
+
+    fn update_error(&mut self, measurement: &[f64; N]) {
+        for i in 0..N {
+            self.last_error[i] = self.setpoint[i] - measurement[i];
+        }
     }
 
     pub fn next_control_output(
         &mut self,
-        measurement: f32,
+        measurement: &[f64; N],
         dt: CuDuration,
     ) -> MPCControlOutputPayload {
         self.elapsed += dt;
 
-        if self.elapsed < self.sampling {
+        if self.elapsed < self.sample_period {
             // if we update the MPC controller too fast, return its previous output
             return self.last_output.clone();
         }
 
-        let error = self.setpoint - measurement;
+        self.update_error(measurement);
         let CuDuration(elapsed) = self.elapsed;
-        let dt = elapsed as f32 / 1_000_000f32; // the unit is kind of arbitrary.
+        let dt = elapsed as f32 / 1_000_000f32;
 
-        // Proportional term
-        let p_unbounded = self.kp * error;
-        let p = p_unbounded.clamp(-self.p_limit, self.p_limit);
+        // do MPC calculation
 
-        // Integral term (accumulated over time)
-        self.integral += error * dt;
-        let i_unbounded = self.ki * self.integral;
-        let i = i_unbounded.clamp(-self.i_limit, self.i_limit);
-
-        // Derivative term (rate of change)
-        let derivative = (error - self.last_error) / dt;
-        let d_unbounded = self.kd * derivative;
-        let d = d_unbounded.clamp(-self.d_limit, self.d_limit);
-
-        // Update last error for next calculation
-        self.last_error = error;
-
-        // Final output: sum of P, I, D with output limit
-        let output_unbounded = p + i + d;
-        let output = output_unbounded.clamp(-self.output_limit, self.output_limit);
-
-        let output = MPCControlOutputPayload { p, i, d, output };
+        let output = MPCControlOutputPayload { output: vec![] };
 
         self.last_output = output.clone();
         self.elapsed = CuDuration::default();
@@ -124,21 +115,20 @@ impl MPCController {
 }
 
 /// This is the Copper task encapsulating the MPC controller.
-pub struct GenericMPCTask<I>
+pub struct GenericMPCTask<I, const N: usize>
 where
-    f32: for<'a> From<&'a I>,
+    [f64; N]: for<'a> From<&'a I>,
 {
     _marker: PhantomData<I>,
-    mpc: MPCController,
+    mpc: MPCController<N>,
     first_run: bool,
     last_tov: CuTime,
     setpoint: f32,
-    cutoff: f32,
 }
 
-impl<I> CuTask for GenericMPCTask<I>
+impl<I, const N: usize> CuTask for GenericMPCTask<I, N>
 where
-    f32: for<'a> From<&'a I>,
+    [f64; N]: for<'a> From<&'a I>,
     I: CuMsgPayload,
 {
     type Input<'m> = input_msg!(I);
@@ -149,60 +139,7 @@ where
         Self: Sized,
     {
         match config {
-            Some(config) => {
-                debug!("MPCTask config: {:?}", config);
-                let setpoint: f32 = config
-                    .get::<f64>("setpoint")
-                    .ok_or("'setpoint' not found in config")?
-                    as f32;
-
-                let cutoff: f32 = config.get::<f64>("cutoff").ok_or(
-                    "'cutoff' not found in config, please set an operating +/- limit on the input.",
-                )? as f32;
-
-                // p is mandatory
-                let kp = if let Some(kp) = config.get::<f64>("kp") {
-                    Ok(kp as f32)
-                } else {
-                    Err(CuError::from(
-                        "'kp' not found in the config. We need at least 'kp' to make the MPC algorithm work.",
-                    ))
-                }?;
-
-                let p_limit = getcfg(config, "pl", 2.0f32);
-                let ki = getcfg(config, "ki", 0.0f32);
-                let i_limit = getcfg(config, "il", 1.0f32);
-                let kd = getcfg(config, "kd", 0.0f32);
-                let d_limit = getcfg(config, "dl", 2.0f32);
-                let output_limit = getcfg(config, "ol", 1.0f32);
-
-                let sampling = if let Some(value) = config.get::<u32>("sampling_ms") {
-                    CuDuration::from(value as u64 * 1_000_000u64)
-                } else {
-                    CuDuration::default()
-                };
-
-                let mpc: MPCController = MPCController::new(
-                    kp,
-                    ki,
-                    kd,
-                    setpoint,
-                    p_limit,
-                    i_limit,
-                    d_limit,
-                    output_limit,
-                    sampling,
-                );
-
-                Ok(Self {
-                    _marker: PhantomData,
-                    mpc,
-                    first_run: true,
-                    last_tov: CuTime::default(),
-                    setpoint,
-                    cutoff,
-                })
-            }
+            Some(_config) => Err(CuError::from("WIP")),
             None => Err(CuError::from("MPCTask needs a config.")),
         }
     }
@@ -215,41 +152,8 @@ where
     ) -> CuResult<()> {
         match input.payload() {
             Some(payload) => {
-                let tov = match input.tov {
-                    Tov::Time(single) => single,
-                    _ => return Err("Unexpected variant for a TOV of MPC".into()),
-                };
-
-                let measure: f32 = payload.into();
-
-                if self.first_run {
-                    self.first_run = false;
-                    self.last_tov = tov;
-                    self.mpc.init_measurement(measure);
-                    output.clear_payload();
-                    return Ok(());
-                }
-                let dt = tov - self.last_tov;
-                self.last_tov = tov;
-
-                // update the status of the MPC controller.
-                let state = self.mpc.next_control_output(measure, dt);
-                // But safety check if the input is within operational margins and cut power if it is not.
-                if measure > self.setpoint + self.cutoff {
-                    return Err(
-                        format!("{} > {} (cutoff)", measure, self.setpoint + self.cutoff).into(),
-                    );
-                }
-                if measure < self.setpoint - self.cutoff {
-                    return Err(
-                        format!("{} < {} (cutoff)", measure, self.setpoint - self.cutoff).into(),
-                    );
-                }
-                output.metadata.set_status(format!(
-                    "{:>5.2} {:>5.2} {:>5.2} {:>5.2}",
-                    &state.output, &state.p, &state.i, &state.d
-                ));
-                output.set_payload(state);
+                // WIP
+                output.clear_payload()
             }
             None => output.clear_payload(),
         };
@@ -264,32 +168,17 @@ where
 }
 
 /// Store/Restore the internal state of the MPC controller.
-impl<I> Freezable for GenericMPCTask<I>
+impl<I, const N: usize> Freezable for GenericMPCTask<I, N>
 where
-    f32: for<'a> From<&'a I>,
+    [f64; N]: for<'a> From<&'a I>,
 {
-    fn freeze<E: Encoder>(&self, encoder: &mut E) -> Result<(), EncodeError> {
-        Encode::encode(&self.mpc.integral, encoder)?;
-        Encode::encode(&self.mpc.last_error, encoder)?;
-        Encode::encode(&self.mpc.elapsed, encoder)?;
-        Encode::encode(&self.mpc.last_output, encoder)?;
+    fn freeze<E: Encoder>(&self, _encoder: &mut E) -> Result<(), EncodeError> {
+        // WIP
         Ok(())
     }
 
-    fn thaw<D: Decoder>(&mut self, decoder: &mut D) -> Result<(), DecodeError> {
-        self.mpc.integral = Decode::decode(decoder)?;
-        self.mpc.last_error = Decode::decode(decoder)?;
-        self.mpc.elapsed = Decode::decode(decoder)?;
-        self.mpc.last_output = Decode::decode(decoder)?;
+    fn thaw<D: Decoder>(&mut self, _decoder: &mut D) -> Result<(), DecodeError> {
+        // WIP
         Ok(())
-    }
-}
-
-// Small helper because we do this again and again
-fn getcfg(config: &ComponentConfig, key: &str, default: f32) -> f32 {
-    if let Some(value) = config.get::<f64>(key) {
-        value as f32
-    } else {
-        default
     }
 }
